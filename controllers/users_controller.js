f5460d018a51964618e05eea87450e0b4964e898
Rishabh Sahay <rishabhsahaya@gmail.com>
2022-09-07 20:59:01 +0530

    manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,90 @@
+const { response } = require("express");
+const { UserInfo } = require("git");
+
+const User = require('../models/user')
+
 module.exports.profile = function(req, res){
-    res.end('<h1>User Profile</h1>');
+
+  /*/ return res.render('user_profile',{
+        title: "User1 Profile"
+   }); This was the previous module part/*/
+
+   if(req.cookies.user_id){
+      User.findById(req.cookies.user_id, function(err, user){
+         if(user){
+            return res.render('user_profile',{
+               title:"User Profile",
+               user:user
+            })
+         }
+         return res.redirect('/users/sign-in');
+      });
+   }else{
+      return res.redirect('/users/sign-in');
+   }
+
+}
+
+//render the sign up page
+module.exports.signUp = function(req, res){
+   return res.render('user_sign_up',{
+      title: "Codeial | Sign Up"
+   })
+}
+
+//render the sign in page
+module.exports.signIn = function(req, res){
+   return res.render('user_sign_in',{
+      title: "Codeial | Sign In"
+   })
+}
+
+//get the sign up data
+module.exports.create = function(req,res){
+   if(req.body.password != req.body.confirm_password){
+      return res.redirect('back');
+   }
+
+   User.findOne({email: req.body.email}, function(err, user){
+      if(err){console.log('error in finding user in signing up'); return}
+
+      if(!user){
+         User.create(req.body, function(err, user){
+            if(err){console.log('error in creaating user while signing up'); return}
+            
+            return res.redirect('/users/sign-in')
+         })
+      }else{
+         return res.redirect('back');
+      }
+
+   });
+
+}
+
+//sign in and create a session for the user
+module.exports.createSession = function(req, res){
+   //find the user
+   User.findOne({email: req.body.email}, function(err, user){
+      if(err){console.log('error in sigining in'); return}
+
+      //handle user found
+      if(user){
+         //handle pasword does not match
+         if(user.password != req.body.password){
+            return res.redirect('back');
+         }
+
+         //handle session creation
+
+         res.cookie('user_id', user.id);
+         return res.redirect('/users/profile');
+      }else{
+         //handle user not found
+
+         return res.redirect('back');
+      }
+
+   });
+
 }
